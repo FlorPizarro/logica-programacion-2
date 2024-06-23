d18230f26f52e3c0def860b90523d7df65ca4e1e
Florencia Pizarro <florenciapizarro.08@gmail.com>
2024-06-23 19:34:14 -0300

    agrego funcion al hacer click

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ titulo.innerHTML='Juego del  numero secreto';
 
 let parrafo= document.querySelector('p');
 parrafo.innerHTML='Ingresa un numero del 1 al 10';
+
+function intentoDeUsuario(){
+    alert('Click desde el botón');
+}
