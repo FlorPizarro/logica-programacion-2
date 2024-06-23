28d24c15eb67080f3c382b794053274700222ca1
Florencia Pizarro <florenciapizarro.08@gmail.com>
2024-06-23 19:18:11 -0300

    agrego mensaje

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 //creo objeto titulo conecto atravez del DOM document
 let titulo = document.querySelector('h1'); 
-titulo.innerHTML='Juego del  numero secreto'
+titulo.innerHTML='Juego del  numero secreto';
+
+let parrafo= document.querySelector('p');
+parrafo.innerHTML='Ingresa un numero del 1 al 10';
