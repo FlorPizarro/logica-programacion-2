bd75d0383b995dda5283463d1f8584e0b26061d0
Florencia Pizarro <florenciapizarro.08@gmail.com>
2024-06-29 15:32:14 -0300

    Actualizo funciones de asignación de texto

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 //creo objeto titulo conecto atravez del DOM document
-let titulo = document.querySelector('h1'); 
-titulo.innerHTML='Juego del  numero secreto';
 
-let parrafo= document.querySelector('p');
-parrafo.innerHTML='Ingresa un numero del 1 al 10';
+/* let parrafo= document.querySelector('p');
+parrafo.innerHTML='Ingresa un numero del 1 al 10'; */
 
 function intentoDeUsuario(){
     alert('Click desde el botón');
 }
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML= document.querySelector(elemento); 
+    elementoHTML.innerHTML= texto;
+}
+
+asignarTextoElemento('h1',"Juego del numero secreto actualizado 3");
+asignarTextoElemento('p','Ingresa un numero del 1 al 10');
